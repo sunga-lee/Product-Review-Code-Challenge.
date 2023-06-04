b65a81a292911a9cf965d811bc4a730ea0dec0a2
sunga-lee <sungajunior1@gmail.com>
2023-06-04 11:31:58 +0300

    updated

---
+++ Gemfile.rb
@@ -0,0 +1,19 @@
+source "https://rubygems.org"
+
+# An object-relational mapper
+# https://guides.rubyonrails.org/active_record_basics.html
+gem "activerecord", "~> 6.1"
+
+# Configures common Rake tasks for working with Active Record
+gem "sinatra-activerecord"
+
+# Run common tasks from the command line
+gem "rake"
+
+# Provides functionality to interact with a SQLite3 database
+gem "sqlite3", "~> 1.4"
+
+# Require all files in a folder
+gem "require_all"
+
+gem "pry"
